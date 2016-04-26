188ff0064d2e6e7903df60f968c3ad69d5696c14
Garret Blevins <gblevins7@utexas.edu>
2016-04-26 14:24:51 -0500

    GUI stuff

--- src/mastermind/MastermindGUI.java
+++ src/mastermind/MastermindGUI.java
@@ -1,14 +1,23 @@
 package mastermind;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class MastermindGUI extends Application implements MastermindConsole {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage stage) throws Exception {
 		
+		Circle circ = new Circle(40, 40, 30);
+        Group root = new Group(circ);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("My JavaFX Application");
+        stage.setScene(scene);
+        stage.show();
 	}
 
 }
