53bc20cea20f2311c7e1ec51d8da8f108d5676df
malvikagupta <malvika.gupta314@gmail.com>
2016-04-25 23:11:10 -0500

    generate code + arrays
    more basic stuff

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -9,11 +9,39 @@ import java.util.Map;
 
 public class Mastermind 
 {
+	int[] historyArray  = new int[48];
+	int[] pegsArray = new int[48];
+	int[] secretCode = new int[4];
 	
+	//initializes both the history and pegs
+	public void initBoard()
+	{
+		for(int i = 0; i< 48; i++)
+		{
+			historyArray[i] = -1;			//setting it to -1 sets it to the default color
+			pegsArray[i] = -1;
+		}
+	}
+	
+	//generates the secret code
+	public void makeSecretCode()
+	{
+		int randInt = (int)(Math.random()*600);
+		secretCode[0] = processColors.randToColor(randInt);
+		randInt = (int)(Math.random()*600);
+		secretCode[1] = processColors.randToColor(randInt);
+		randInt = (int)(Math.random()*600);
+		secretCode[2] = processColors.randToColor(randInt);
+		randInt = (int)(Math.random()*600);
+		secretCode[3] = processColors.randToColor(randInt);
+	}
+	
+	//resets the board
+	{
+		initBoard();
+		makeSecretCode();
+	}
 
-	//hash map of ints to color
-	Map<Integer, String> colors = new HashMap<Integer, String>();
-	colors.put(new Integer(0),"t");
 	
 }
 
--- src/mastermind/processColors.java
+++ src/mastermind/processColors.java
@@ -1,34 +1,88 @@
 package mastermind;
+/*
+ * GENERAL NOTES
+ * make the default color the same color as the background for a cleaner look
+ * could we possibly make the buttons the color they represent?
+ */
 
 import java.awt.Color;
 
 public class processColors {
 	Color purple = new Color(160, 32, 240);
-	public Color process(int colorInt){
+	
+	
+	public static Color processHistory(int colorInt){
 		if(colorInt == 0)
 		{
 			return Color.blue;
 		}
-		if(colorInt == 0)
+		if(colorInt == 1)
 		{
 			return Color.green;
 		}
-		if(colorInt == 0)
+		if(colorInt == 2)
 		{
 			return Color.orange;
 		}
-		if(colorInt == 0)
+		if(colorInt == 3)
 		{
 			return purple;
 		}
-		if(colorInt == 0)
+		if(colorInt == 4)
 		{
 			return Color.red;
 		}
-		if(colorInt == 0)
+		if(colorInt == 5)
 		{
 			return Color.orange;
 		}
 		return Color.DARK_GRAY;
 	}
+	
+	public static Color processPegs(int colorInt)
+	{
+		if(colorInt == 6)
+		{
+			return Color.white; 	//correct color, not the right place
+		}
+		if(colorInt == 7)
+		{
+			return Color.black;		//correct color, correct place
+		}
+		return Color.DARK_GRAY;
+	}
+	
+	public static int randToColor(int rand)
+	{
+		if(rand<100)
+		{
+			return 0;
+		}
+		if(rand<100)
+		{
+			return 1;
+		}
+		if(rand<100)
+		{
+			return 2;
+		}
+		if(rand<100)
+		{
+			return 3;
+		}
+		if(rand<100)
+		{
+			return 4;
+		}
+		if(rand<100)
+		{
+			return 5;
+		}
+		else
+		{
+			System.err.println("You have an issue with randToColor");
+			return 6;
+		}
+	}
+	
 }
